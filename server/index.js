1b3cf74087a05eed926f3c42a82b414d0be46931
䂊汐 <28.juxi.lu@gmail.com>
2019-04-25 00:01:35 -0400

    parsing and mongodb, bad words filter

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+const Filter = require('bad-words');
+
+const app = express();
+
+const db = monk('localhost/meower');
+const mews = db.get('mews');
+const filter = new Filter();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	message: 'Meower'
+});
+
+app.get('/mews', (req, res) => {
+	mews
+		.find()
+		.then(mews => {
+			res.json(mews);
+		});
+});
+
+function isValidMew(mew) {
+	return mew.name && mew.name.toString().trim() !== '' &&
+	mew.content && mew.content.toString().trim() !== '';
+}
+
+app.post('/mews', (req, res) => {
+	if (isValidMew(req.body)) {
+		const mew = {
+			name: filter.clean(req.body.name.toString()),
+			content: filter.clean(req.body.content.toString()),
+			created: new Date()
+		};
+		mews
+			.insert(mew)
+			.then(createdMew => {
+				res.json(createdMew);
+			});
+	} else {
+		res.status(422);
+		res.json({
+			message: 'Hey! Name and Content are required!'
+		});
+	}
+});
+
+app.listen(5000, () => {
+	console.log('Listening on http://localhost:5000');
+});
