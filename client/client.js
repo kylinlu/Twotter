f0205ac0cd12c398472aec9f9cdce331d7cb416a
䂊汐 <28.juxi.lu@gmail.com>
2019-04-29 17:21:17 -0400

    cleanup

--- client/client.js
+++ client/client.js
@@ -1,96 +1,115 @@
-const form = document.querySelector('form');
+const form = document.querySelector('form'); // grabbing an element on the page
 const errorElement = document.querySelector('.error-message');
 const loadingElement = document.querySelector('.loading');
-const mewsElement = document.querySelector('.mews');
+const twotsElement = document.querySelector('.twots');
 const loadMoreElement = document.querySelector('#loadMore');
-const API_URL = windo.location.hostname == 'localhost' ? 'http://localhost:5000/v2/mews' : 'https://meower-api.now.sh/v2/mews';
+const API_URL = window.location.hostname === '127.0.0.1' ? 'http://localhost:5000/v2/twots' : 'https://twotter-api.now.sh/v2/twots';
 
 let skip = 0;
-let limit = 10;
+let limit = 5;
 let loading = false;
 let finished = false;
+
 errorElement.style.display = 'none';
 
-document.addEventListener('scroll' () => {
-	const rect = loadMoreElement.getBoundingClientRect();
-	if (rect.top < window.innerHeight && !loading && !finished) {
-		loadMore();
-	}
+document.addEventListener('scroll', () => {
+  const rect = loadMoreElement.getBoundingClientRect();
+  if (rect.top < window.innerHeight && !loading && !finished) {
+    loadMore();
+  }
 });
 
-listAllMews();
+listAllTwots();
 
 form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const formData = new FormData(form);
-	const name = formData.get('name');
-	const content = formData.get('content');
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
 
-	const mew = {
-		mew,
-		content
-	};
-	form.style.display = 'none';
-	loadingElement.style.display = '';
+  if (name.trim() && content.trim()) {
+    errorElement.style.display = 'none';
+    form.style.display = 'none';
+    loadingElement.style.display = '';
 
-	fetch(API_URL, {
-		method: 'POST',
-		body: JSON.stringify(mew),
-		headers: {
-			'content-type': 'application/json'
-		}
-	}).then(response => response.json())
-	  .then(createdMew => {
-	  	form.reset();
-	  	setTimeout(() => {
-	  		form.style.display = '';
-	  	}, 5000);
-	  	form.style.display = '';
-	  	listAllMews();
-	  	loadingElement.style.display = 'none';
-	  });
+    const twot = {
+      name,
+      content
+    };
+    
+    fetch(API_URL, {
+      method: 'POST',
+      body: JSON.stringify(twot),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => {      
+      if (!response.ok) {
+        const contentType = response.headers.get('content-type');
+        if (contentType.includes('json')) {
+          return response.json().then(error => Promise.reject(error.message));
+        } else {
+          return response.text().then(message => Promise.reject(message));
+        }
+      }
+    }).then(() => {
+      form.reset();
+      setTimeout(() => {
+        form.style.display = '';
+      }, 30000);
+      listAllTwots();
+    }).catch(errorMessage => {
+      form.style.display = '';
+      errorElement.textContent = errorMessage;
+      errorElement.style.display = '';
+      loadingElement.style.display = 'none';
+    });
+  } else {
+    errorElement.textContent = 'Name and content are required!';
+    errorElement.style.display = '';
+  }
 });
 
 function loadMore() {
-	skip += limit;
-	listAllMews(false);
+  skip += limit;
+  listAllTwots(false);
 }
 
-function listAllMews(reset = true) {
-	loading = true;
-	if (reset) {
-		mewsElement.innerHTML = '';
-		skip = 0;
-		finished = false;
-	}
-	mewsElement.innerHTML = '';
-	fetch(API_URL)
-		.then(response => response.json())
-		.then(result => {
-			if (result.meta.has_more) {
-				loadMoreButton.style.visibility = 'hidden';
-			}
-			mews.forEach(mew => {
-				const div = document.createElement('div');
-				const header = document.createElement('h3');
-				header.textContent = mew.name;
-				const contents = document.createElement('p');
-				contents.textContent = mew.content;
-				const date = document.createElement('small');
-				date.textContent = new Date(mew.created);
+function listAllTwots(reset = true) {
+  loading = true;
+  if (reset) {
+    twotsElement.innerHTML = '';
+    skip = 0;
+    finished = false;
+  }
+  fetch(`${API_URL}?skip=${skip}&limit=${limit}`)
+    .then(response => response.json())
+    .then(result => {
+      result.twots.forEach(twot => {
+        const div = document.createElement('div');
 
-				div.appendChild(header);
-				div.appendChild(contents);
-				div.appendChild(date);
-				mewsElement.appendChild(div);
-			});
-			loadingElement.style.display = 'none';
-			if (!result.meta.has_more) {
-				loadMoreElement.style.visibility = 'hidden';
-				finished = true;
-			} else {
-				loadMoreElement.style.visibility = 'visible';
-			}
-			loading = false;
-		});
+        const header = document.createElement('h3');
+        header.textContent = twot.name;
+
+        const contents = document.createElement('p');
+        contents.textContent = twot.content;
+
+        const date = document.createElement('small');
+        date.textContent = new Date(twot.created);
+
+        div.appendChild(header);
+        div.appendChild(contents);
+        div.appendChild(date);
+
+        twotsElement.appendChild(div);
+      });
+      loadingElement.style.display = 'none';
+      if (!result.meta.has_more) {
+        loadMoreElement.style.visibility = 'hidden';
+        finished = true;
+      } else {
+        loadMoreElement.style.visibility = 'visible';
+      }
+      loading = false;
+    });
 }
--- server/index.js
+++ server/index.js
@@ -6,86 +6,105 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-const db = monk(process.env.MONGO_URI || 'localhost/meower');
-const mews = db.get('mews');
+const db = monk(process.env.MONGO_URI || 'localhost/twotter');
+const twots = db.get('twots');
 const filter = new Filter();
+
+app.enable('trust proxy');
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	message: 'Meower'
+  res.json({
+    message: 'Meower! 😹 🐈'
+  });
 });
 
-app.get('/mews', (req, res) => {
-	mews
-		.find()
-		.then(mews => {
-			res.json(mews);
-		}).catch(next);
+app.get('/twots', (req, res, next) => {
+  twots
+    .find()
+    .then(twots => {
+      res.json(twots);
+    }).catch(next);
 });
 
-app.get('/v2/mews', (req, res) => {
-	let { skip = 0, limit = 10, sort = 'desc' } = req.query;
-	skip = parseInt(skip) || 0;
-	limit = parseInt(limit) || 10;
-	skip = skip < 0 ? 0 : skip
-	limt = Math.min(50, Math.max(1, limit));
-	Promise.all([
-		mews.
-			count(),
-		mews
-			.find({}, {
-				skip,
-				limit,
-				sort: {
-					created:  sort === 'desc' ? -1 : 1
-				}
-			})
-	])
-		.then(([ total, mews ]) => {
-			res.json({
-				total,
-				mews,
-				meta: {
-					skip,
-					limit,
-					has_more: total - (skip + limit) > 0,
-				}
-			});
-		}).catch(next);
+app.get('/v2/twots', (req, res, next) => {
+  // let skip = Number(req.query.skip) || 0;
+  // let limit = Number(req.query.limit) || 10;
+  let { skip = 0, limit = 5, sort = 'desc' } = req.query;
+  skip = parseInt(skip) || 0;
+  limit = parseInt(limit) || 5;
+
+  skip = skip < 0 ? 0 : skip;
+  limit = Math.min(50, Math.max(1, limit));
+
+  Promise.all([
+    twots
+      .count(),
+    twots
+      .find({}, {
+        skip,
+        limit,
+        sort: {
+          created: sort === 'desc' ? -1 : 1
+        }
+      })
+  ])
+    .then(([ total, twots ]) => {
+      res.json({
+        twots,
+        meta: {
+          total,
+          skip,
+          limit,
+          has_more: total - (skip + limit) > 0,
+        }
+      });
+    }).catch(next);
 });
 
-function isValidMew(mew) {
-	return mew.name && mew.name.toString().trim() !== '' &&
-	mew.content && mew.content.toString().trim() !== '';
+function isValidTwot(twot) {
+  return twot.name && twot.name.toString().trim() !== '' && twot.name.toString().trim().length <= 50 &&
+    twot.content && twot.content.toString().trim() !== '' && twot.content.toString().trim().length <= 140;
 }
 
 app.use(rateLimit({
-	windowMs: 5 * 1000, //1 request every 5 seconds limit
-	max: 1
+  windowMs: 30 * 1000, // 30 seconds
+  max: 1
 }));
 
-app.post('/mews', (req, res) => {
-	if (isValidMew(req.body)) {
-		const mew = {
-			name: filter.clean(req.body.name.toString()),
-			content: filter.clean(req.body.content.toString()),
-			created: new Date()
-		};
-		mews
-			.insert(mew)
-			.then(createdMew => {
-				res.json(createdMew);
-			});
-	} else {
-		res.status(422);
-		res.json({
-			message: 'Hey! Name and Content are required!'
-		});
-	}
+const createTwot = (req, res, next) => {
+  if (isValidTwot(req.body)) {
+    const twot = {
+      name: filter.clean(req.body.name.toString().trim()),
+      content: filter.clean(req.body.content.toString().trim()),
+      created: new Date()
+    };
+
+    twots
+      .insert(twot)
+      .then(createdTwot => {
+        res.json(createdTwot);
+      }).catch(next);
+  } else {
+    res.status(422);
+    res.json({
+      message: 'Hey! Name and Content are required! Name cannot be longer than 50 characters. Content cannot be longer than 140 characters.'
+    });
+  }
+};
+
+app.post('/twots', createTwot);
+app.post('/v2/twots', createTwot);
+
+app.use((error, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: error.message
+  });
 });
 
 app.listen(5000, () => {
-	console.log('Listening on http://localhost:5000');
+  console.log('Listening on http://localhost:5000');
 });
